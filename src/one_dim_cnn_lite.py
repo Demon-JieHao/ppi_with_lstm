09e9d155c1a6698d94317cc3233bb5c4e7c7573a
Giovanni d'Ario <giovanni.dario@novartis.com>
2017-09-18 13:39:40 +0200

    Add the full paths

--- src/one_dim_cnn_lite.py
+++ src/one_dim_cnn_lite.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 
 now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-log_dir = 'tf_logs/cnn_1d' + now
+log_dir = '/da/dmp/cb/dariogi1/projects/2017/ppi_with_lstm/src/tf_logs/cnn_1d' + now
 
 max_protein_length = 500
 n_aas = 21
@@ -20,8 +20,8 @@ conv1_pad = 'VALID'
 n_fc1 = 256
 n_fc2 = 128
 
-n_epochs = 30
-learning_rate = 0.01
+n_epochs = 50
+learning_rate = 0.001
 
 batch_size = 128
 
@@ -70,7 +70,7 @@ def batch_generator(x1, x2, y, batch_size, how,
 
 
 # Load the dataset still in a list form
-X1, X2, Y = pickle.load(gzip.open('../output/create_dataset.pkl.gzip', 'r'))
+X1, X2, Y = pickle.load(gzip.open('/da/dmp/cb/dariogi1/projects/2017/squads/ppi_with_lstm/output/create_dataset.pkl.gzip', 'r'))
 # Y = Y.reshape(Y.shape[0], 1)
 
 # Create training, dev and test set
